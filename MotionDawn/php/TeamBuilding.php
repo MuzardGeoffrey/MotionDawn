b816f08bf9f603d3d2a23775baf6b6368a953d52
Unknown <shadowhite@hotmail.fr>
2019-01-23 18:45:57 +0100

    site a jour 0.2

--- MotionDawn/php/TeamBuilding.php
+++ MotionDawn/php/TeamBuilding.php
@@ -12,7 +12,7 @@ include 'nav.php';
             <h2 class="TEvenement">Team Building</h2>
             <p class="PEvenement">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
             <img src="img/Toutou.jpg" id="inauguration">
-        </div>'
+        </div>';
 // <!------------------------------------------------------->
 // <!--                      FOOTER                       -->
 // <!------------------------------------------------------->
